d3b1420f9a7b5f8161391e1d852b6cc987f2e526
Joshua Namanga <namangaj@gmail.com>
2024-04-07 09:01:24 -0700

    edited task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-Fetches https://alx-intranet.hbtn.io/status and displays the body of the response
+Fetches http://0.0.0.0:5050/status and displays the body of the response
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    url = "http://0.0.0.0:5050/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         html = response.read()
+        utf8_content = html.decode("utf-8")
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(utf8_content))
