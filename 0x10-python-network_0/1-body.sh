f49c3ae3dc424126ce84f8fc28261d99d5393a37
Joshua Namanga <namangaj@gmail.com>
2024-04-07 07:55:23 -0700

    edited task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to that URL using curl in silent mode, and displays the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -f "$1"
