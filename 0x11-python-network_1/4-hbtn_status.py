5f138dea433c95aacfd4b8513d1a2d1292d96ac5
Joshua Namanga <namangaj@gmail.com>
2024-04-07 09:30:39 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status and displays the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
