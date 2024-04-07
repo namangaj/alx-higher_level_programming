3dfcc1ace1838c88c9d93e7f64544ad1ef0d3e9f
Joshua Namanga <namangaj@gmail.com>
2024-04-07 08:33:08 -0700

    Edited task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,17 @@ def find_peak(list_of_integers):
         return None
     
     peaks = []
-    n = len(list_of_integers)
     
     # Check if the first element is a peak
-    if n == 1 or list_of_integers[0] >= list_of_integers[1]:
+    if len(list_of_integers) >= 1 and list_of_integers[0] >= list_of_integers[1]:
         peaks.append(list_of_integers[0])
     
     # Check if the last element is a peak
-    if n >= 2 and list_of_integers[n - 1] >= list_of_integers[n - 2]:
-        peaks.append(list_of_integers[n - 1])
+    if len(list_of_integers) >= 2 and list_of_integers[-1] >= list_of_integers[-2]:
+        peaks.append(list_of_integers[-1])
     
-    # Check other elements for peaks
-    for i in range(1, n - 1):
+    # Iterate through the list and check for peaks
+    for i in range(1, len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
             peaks.append(list_of_integers[i])
     
