2c30d3d73840195a938054ccb31e76c9211ef540
Joshua Namanga <namangaj@gmail.com>
2024-04-07 08:15:45 -0700

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-# Bash script to send a POST request to a URL with specified variables and display the body of the response
-
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL from command line argument
-url="$1"
-
-# Variables to be sent in the POST request
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send a POST request to the URL with the specified variables using curl and display the body of the response
-curl_output=$(curl -s -X POST -d "email=$email&subject=$subject" "$url")
-
-# Display the body of the response
-echo "Body of the response:"
-echo "$curl_output"
+# This script takes in a URL, sends a POST request with specific parameters, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
