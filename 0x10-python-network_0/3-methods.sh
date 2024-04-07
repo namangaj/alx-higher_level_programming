5f51ffe98f2ae26ef31367148e5431d6837b00a2
Joshua Namanga <namangaj@gmail.com>
2024-04-07 08:10:11 -0700

    edited task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-!/bin/bash
+#!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow" | sed 's/Allow: //'
+curl -sI "$1" | grep -i "Allow" | cut -d' ' -f2-
