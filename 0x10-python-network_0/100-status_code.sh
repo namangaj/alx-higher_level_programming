53aa4c992adb49b282f8567c809f7ebc0de9cc20
Joshua Namanga <namangaj@gmail.com>
2024-04-07 08:35:18 -0700

    Task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# Bash script to send a request to a URL and display only the status code of the response
 
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL from command line argument
-url="$1"
-
-# Send a request to the URL using curl and display only the status code of the response
-curl -s -o /dev/null -w "%{http_code}" "$url"
+# This script sends a request to a URL passed as an argument and displays only the status code of the response
+curl -s -o /dev/null -w "%{http_code}" "$1"
