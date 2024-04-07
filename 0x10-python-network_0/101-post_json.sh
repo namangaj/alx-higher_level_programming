0a049ae1263486961f3361e2780928d767f7b3de
Joshua Namanga <namangaj@gmail.com>
2024-04-07 08:47:50 -0700

    edited task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# This script sends a JSON POST request to a URL with the contents of a file in the body, and displays the response body
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <URL> <file>"
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f "$2" ]; then
+    echo "File '$2' does not exist."
+    exit 1
+fi
+
+# Send the JSON POST request and display the response body
 curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
